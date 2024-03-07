6b1e10d7f54e2b81477206efc3588fbdb383a62d
Chetan Bagri <cbagri@usc.edu>
2024-03-07 13:01:48 -0800

    Working Backend Testing

--- src/test/java/edu/usc/csci310/project/createAccountMyStepdefs.java
+++ src/test/java/edu/usc/csci310/project/createAccountMyStepdefs.java
@@ -8,52 +8,73 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class createAccountMyStepdefs {
-
-    private static final String ROOT_URL = "http://localhost:8080";
     private final WebDriver driver = new ChromeDriver();
 
     @Given("I am on the create account page")
     public void iAmOnTheCreateAccountPage() {
-        driver.get(ROOT_URL);
+        driver.get("http://localhost:8080/create-account");
     }
 
     @When("I enter the username {string}")
     public void iEnterTheUsername(String username) {
-        WebElement usernameInput = driver.findElement(By.cssSelector("input[type='text']"));
+        WebElement usernameInput = driver.findElement(By.id("username"));
         usernameInput.sendKeys(username);
     }
 
     @And("I enter the email {string}")
     public void iEnterTheEmail(String email) {
-        WebElement emailInput = driver.findElement(By.cssSelector("input[type='email']"));
+        WebElement emailInput = driver.findElement(By.id("email"));
         emailInput.sendKeys(email);
     }
 
     @And("I enter the password {string}")
     public void iEnterThePassword(String password) {
-        WebElement passwordInput = driver.findElement(By.cssSelector("input[type='password']:nth-of-type(1)"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
         passwordInput.sendKeys(password);
-    }
-
-    @And("I press the Create Account button")
-    public void iPressTheCreateAccountButton() {
-        WebElement createAccountButton = driver.findElement(By.xpath("//button[contains(text(), 'Create Account')]"));
-        createAccountButton.click();
-    }
-
-    @Then("I should get a {string} message")
-    public void iShouldGetAMessage(String message) throws InterruptedException {
-        Thread.sleep(10000);
-        assertTrue(driver.getPageSource().contains(message));
     }
 
     @And("I confirm the password {string}")
     public void iConfirmThePassword(String confirmPassword) {
-        WebElement confirmPasswordInput = driver.findElement(By.cssSelector("input[type='password']:nth-of-type(2)"));
+        WebElement confirmPasswordInput = driver.findElement(By.id("confirmPassword"));
         confirmPasswordInput.sendKeys(confirmPassword);
     }
+
+    @And("I press the Create Account button")
+    public void iPressTheCreateAccountButton() {
+        WebElement createAccountButton = driver.findElement(By.id("submission"));
+        createAccountButton.click();
+    }
+
+    @Then("I should get a {string} message")
+    public void iShouldGetAMessage(String message) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/p")));
+        WebElement messageElement = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/p"));
+        assertEquals(message, messageElement.getText());
+        driver.quit();
+    }
+
+    @And("I should be redirected to the Landing Page")
+    public void iShouldBeRedirectedToTheLandingPage() {
+        driver.get("http://localhost:8080/landing");
+        driver.quit();
+    }
+
+    @Then("I should get an alert saying {string}")
+    public void iShouldGetAnAlertSaying(String alertMessage) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        String alertText = alert.getText();
+        assertEquals(alertMessage, alertText);
+        alert.accept();
+    }
+
 }
--- src/test/resources/edu/usc/csci310/project/CreateAccount.feature
+++ src/test/resources/edu/usc/csci310/project/CreateAccount.feature
@@ -3,56 +3,27 @@ Feature: test the create account functionality of website
     Given I am on the create account page
     When I enter the username "Anika"
     And I enter the email "anika@example.com"
-    And I enter the password "password"
-    And I confirm the password "password"
+    And I enter the password "happygolucky"
+    And I confirm the password "happygolucky"
     And I press the Create Account button
-    Then I should get a "Successful Account Creation" message
+    Then I should get an alert saying "Successful Account Creation"
+    And I should be redirected to the Landing Page
 
 
-  Scenario: blank attempt
-    Given I am on the create account page
-    When I enter the username ""
-    And I enter the email ""
-    And I enter the password ""
-    And I confirm the password ""
-    And I press the Create Account button
-    Then I should get a "Failed to create account: username and password required" message
-
-
-  Scenario: empty password
+  Scenario: username exists
     Given I am on the create account page
     When I enter the username "Anika"
+    And I enter the email "jainee@example.com"
+    And I enter the password "happygolucky"
+    And I confirm the password "happygolucky"
+    And I press the Create Account button
+    Then I should get a "Failed to create account: User already exists with this username" message
+
+  Scenario: email exists
+    Given I am on the create account page
+    When I enter the username "Jainee"
     And I enter the email "anika@example.com"
-    And I enter the password ""
-    And I confirm the password ""
+    And I enter the password "happygolucky"
+    And I confirm the password "happygolucky"
     And I press the Create Account button
-    Then I should get a "Failed to create account: password required" message
-
-
-  Scenario: empty username
-    Given I am on the create account page
-    When I enter the username ""
-    And I enter the email "anika@example.com"
-    And I enter the password "password"
-    And I confirm the password "password"
-    And I press the Create Account button
-    Then I should get a "Failed to create account: username required" message
-
-
-  Scenario: empty email
-    Given I am on the create account page
-    When I enter the username "Anika"
-    And I enter the email ""
-    And I enter the password "password"
-    And I confirm the password "password"
-    And I press the Create Account button
-    Then I should get a "Failed to create account: email required" message
-
-  Scenario: password confirmation incorrect
-    Given I am on the create account page
-    When I enter the username "Anika"
-    And I enter the email ""
-    And I enter the password "password"
-    And I confirm the password "passworf"
-    And I press the Create Account button
-    Then I should get a "Passwords do not match." message
+    Then I should get a "Failed to create account: User already exists with this email" message
