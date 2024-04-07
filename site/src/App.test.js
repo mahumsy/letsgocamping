ca5cc5f597eb342a575e42fe29d52b4b237415ea
Anika Patel <anikaptl775@gmail.com>
2024-04-07 13:51:08 -0700

    fixed refactor #41

--- site/src/App.test.js
+++ site/src/App.test.js
@@ -1,9 +1,7 @@
 import React from "react";
-import {render, screen, fireEvent, getByLabelText} from "@testing-library/react"; //remove waitfor
+import {render, screen, fireEvent} from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import userEvent from "@testing-library/user-event";
-import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 afterEach(() => {
@@ -14,66 +12,11 @@ beforeEach(() => {
     fetch.resetMocks();
 });
 
-// test("full login rendering", async () => {
-//     const user = userEvent.setup();
-//     render(<Login />, { wrapper: BrowserRouter });
-//
-//     // verify page content for default route
-//     // expect(screen.getByText(/Login/)).toBeInTheDocument(); // Multiple elements with Login so don't use
-//     expect(screen.getByText(/Username/)).toBeInTheDocument();
-//     expect(screen.getByText(/Password/)).toBeInTheDocument();
-//     expect(screen.getByTestId('test-loginBtn')).toBeInTheDocument();
-// });
-//
-// test("Submit invalid login form and get fecthed response", async () => {
-//     fetch.mockResponseOnce(JSON.stringify({ data: "Login Unsuccessful" }));
-//
-//     const user = userEvent.setup();
-//     render(<Login />, { wrapper: BrowserRouter });
-//
-//     // verify page content for default route
-//
-//     expect(screen.getByText(/Username/)).toBeInTheDocument();
-//     expect(screen.getByText(/Password/)).toBeInTheDocument();
-//     expect(screen.getByTestId('test-loginBtn')).toBeInTheDocument();
-//
-//     // fill out login form
-//     fireEvent.change(screen.getByTestId('test-username'), {target: {value: 'Nick'}});
-//     fireEvent.change(screen.getByTestId('test-password'), {target: {value: 'mypassword'}});
-//
-//     fireEvent.click(screen.getByTestId('test-loginBtn'));
-//
-//     expect(fetch).toHaveBeenCalledTimes(1);
-// });
-//
-// test("Default null fetched response (inputs don't matter)", async () => {
-//     fetch.mockResponseOnce(JSON.stringify({ }));
-//
-//     const user = userEvent.setup();
-//     render(<Login />, { wrapper: BrowserRouter });
-//
-//     // verify page content for default route
-//
-//     expect(screen.getByText(/Username/)).toBeInTheDocument();
-//     expect(screen.getByText(/Password/)).toBeInTheDocument();
-//     expect(screen.getByTestId('test-loginBtn')).toBeInTheDocument();
-//
-//     // fill out login form
-//     fireEvent.change(screen.getByTestId('test-username'), {target: {value: 'Nick'}});
-//     fireEvent.change(screen.getByTestId('test-password'), {target: {value: 'mypassword'}});
-//
-//     fireEvent.click(screen.getByTestId('test-loginBtn'));
-//
-//     expect(fetch).toHaveBeenCalledTimes(1);
-// });
 
 test("App.js file coverage", async () => {
-    const user = userEvent.setup();
     render(<App />, { wrapper: BrowserRouter }); // Triggers App.js coverage but defaults to Home.jsx
 
     // verify page content for default route
-    // expect(screen.getByTestId("test-login")).toBeInTheDocument();
-    // expect(screen.getByText(/CreateAccount/i)).toBeInTheDocument();
     expect(screen.getByText("Username:")).toBeInTheDocument();
 
 });
